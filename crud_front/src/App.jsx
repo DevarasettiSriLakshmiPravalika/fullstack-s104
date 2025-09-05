da8bfa3a8bb7c89caf368365737c531d105fc645
DevarasettiSriLakshmiPravalika <2300033011cseh@gmail.com>
2025-09-05 21:40:50 +0530

    Update App.jsx

--- crud_front/src/App.jsx
+++ crud_front/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   const handleChange = (e) => {
     setProduct({ ...product, [e.target.name]: e.target.value });
   };
-const BASE_URL = 'http://51.21.69.126:9090/springapp1';
+const BASE_URL = 'http://16.16.112.189:9090/springapp1';
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
